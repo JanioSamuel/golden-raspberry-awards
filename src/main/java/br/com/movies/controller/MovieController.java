3cd677fc4e79bbe3007c5a4bfb131903d86a64c5
Janio Samuel <samucagomes08@gmail.com>
2021-02-07 13:39:37 -0300

    Ajustados endpoints

--- src/main/java/br/com/movies/controller/MovieController.java
+++ src/main/java/br/com/movies/controller/MovieController.java
@@ -6,24 +6,26 @@ import br.com.movies.model.Movie;
 import br.com.movies.model.WinnerResponse;
 import br.com.movies.service.MovieService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/movies")
+@RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:3000")
 public class MovieController {
 
 	@Autowired
 	private MovieService service;
 
-	@GetMapping(value = "")
+	@GetMapping(value = "/movies")
 	public @ResponseBody List<Movie> findAllValues() {
 		return service.findAll();
 	}
 
-	@GetMapping(value = "/producers")
+	@GetMapping(value = "/winners")
 	public @ResponseBody WinnerResponse mostAndMinorIntervals() {
 		return service.findMinimumAndMaximumValues();
 	}
